8c4c090070728e35280a32bd35f90b838e57cfe5
Damien Radtke <damienradtke@gmail.com>
2013-03-24 16:12:27 -0500

    Added common.go.

---
+++ misc/common.go
@@ -0,0 +1,11 @@
+/* Common Go code across all generated bindings */
+
+type GError struct {
+	Code int
+	Message string
+}
+
+func (self GError) Error() string {
+	// ???: include error code?
+	return Message
+}
